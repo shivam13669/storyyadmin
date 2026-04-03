f09624d7019e4f36eb0dc6867c9df336b665b956
Shivam Anand <shivam19e@gmail.com>
2026-04-03 16:22:38 +0530

    Revert "Redesign homepage with Cliffhanger2 layout and components"

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,26 +1,18 @@
 import Navigation from "@/components/Navigation";
-import HeroSlider from "@/components/HeroSlider";
-import CategoryBoxes from "@/components/CategoryBoxes";
-import DiscoverNewHorizons from "@/components/DiscoverNewHorizons";
-import OurServices from "@/components/OurServices";
-import HotTours from "@/components/HotTours";
-import Team from "@/components/Team";
-import ParallaxCTA from "@/components/ParallaxCTA";
-import Gallery from "@/components/Gallery";
+import Hero from "@/components/Hero";
+import Destinations from "@/components/Destinations";
+import HowItWorks from "@/components/HowItWorks";
+import TestimonialsCarousel from "@/components/TestimonialsCarousel";
 import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
       <Navigation />
-      <HeroSlider />
-      <CategoryBoxes />
-      <DiscoverNewHorizons />
-      <OurServices />
-      <HotTours />
-      <Team />
-      <ParallaxCTA />
-      <Gallery />
+      <Hero />
+      <Destinations />
+      <HowItWorks />
+      <TestimonialsCarousel />
       <Footer />
     </div>
   );
